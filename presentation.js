3abe618e6a656e6d0a394c147b285d54adb51256
Hijazi Syed <hijazisyed@gmail.com>
2025-09-01 11:50:03 +0500

    Add initial presentations array to presentation.js

---
+++ presentation.js
@@ -0,0 +1,5 @@
+const presentations = [
+  { title: "LOC", url: "LOC.html", thumbnail: "images/LOC-thumbnail.png" },
+  { title: "Other Presentation", url: "other-file.html", thumbnail: "images/other-file-thumbnail.png" },
+  // Add more presentations as needed
+];
