1bf999bddb2a8daafd6b32fe0b266cd27fc3c210
Hijazi Syed <hijazisyed@gmail.com>
2025-09-01 23:08:35 +0500

    Update presentations.js

--- presentations.js
+++ presentations.js
@@ -4,8 +4,8 @@ const presentations = [
     url: "Pakistan_Cumulative_Price_Index_1947_2025.pdf"
   },
   {
-    title: "Pakistan GDP Growth 1950-2025",
-    url: "Pakistan_GDP_Growth_1950_2025.pdf"
+    title: "Pakistan Inflation Trend 1950-2025",
+    url: "Pakistan_Inflation_Trend_1947_2025.pdf"
   }
   // Add more presentations here if needed.
 ];
