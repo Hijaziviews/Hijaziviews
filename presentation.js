42a21e790441093922803c3d1684d06b7fdaff82
Hijazi Syed <hijazisyed@gmail.com>
2025-09-01 22:15:03 +0500

    Update presentation details and add new entry

--- presentation.js
+++ presentation.js
@@ -1,12 +1,7 @@
 const presentations = [
-  { title: "LOC", url: "LOC.html", thumbnail: "images/LOC-thumbnail.png" },
-  { title: "Other Presentation", url: "other-file.html", thumbnail: "images/other-file-thumbnail.png" },
-  // Add more presentations as needed
-];  const presentations = [
   {
-    title: "Pakistan Inflation Trend (1947-2025)",
-    url: "files/Pakistan_Inflation_Trend_1947_2025.pdf",
-    thumbnail: "images/Pakistan_Inflation_Trend_1947_2025-thumbnail.png"
-  },
-  // ...other presentations
+    title: "Pakistan Cumulative Price Index 1947-2025",
+    url: "Pakistan_Cumulative_Price_Index_1947_2025_with_legend.pdf"
+  }
+  // Add more presentations here if needed.
 ];
