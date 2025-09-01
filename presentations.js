7aa4ef17e6d3579361bde2d4a38735057bbea956
Hijazi Syed <hijazisyed@gmail.com>
2025-09-01 22:57:36 +0500

    Add GDP Growth presentation to presentations list

--- presentations.js
+++ presentations.js
@@ -2,6 +2,10 @@ const presentations = [
   {
     title: "Pakistan Cumulative Price Index 1947-2025",
     url: "Pakistan_Cumulative_Price_Index_1947_2025.pdf"
+  },
+  {
+    title: "Pakistan GDP Growth 1950-2025",
+    url: "Pakistan_GDP_Growth_1950_2025.pdf"
   }
   // Add more presentations here if needed.
 ];
