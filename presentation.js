8cf0d3ea45b94bd677f9236a241315d607d444db
Hijazi Syed <hijazisyed@gmail.com>
2025-09-01 22:34:41 +0500

    Update presentation.js

--- presentation.js
+++ presentation.js
@@ -1,7 +1,7 @@
 const presentations = [
   {
     title: "Pakistan Cumulative Price Index 1947-2025",
-    url: "Pakistan_Cumulative_Price_Index_1947_2025_with_legend.pdf"
+    url: "Pakistan_Cumulative_Price_Index_1947_2025.pdf"
   }
   // Add more presentations here if needed.
 ];
